76f4efe1394b6d19272981686420562848aea294
Stefania <estefansilvi@gmail.com>
2021-02-22 14:05:54 +0100

    footer completo

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var app = new Vue({
+el: '#app',
+data: {
+  navBarList: [
+    {
+      name: 'HOME',
+      link: ''
+    },
+    {
+      name: 'MISSION',
+      link: ''
+    },
+    {
+      name: 'CAUSES',
+      link: ''
+    },
+    {
+      name: 'JOURNAL',
+      link: ''
+    },
+  ],
+  footerList:[
+    {
+      name:'AVADA THEME BY THEME FUSION'
+    },
+    {
+      name:'ALL RIGHT RESERVE'
+    },
+    {
+      name:'POWERED BY WORDPRESS'
+    }
+  ],
+}
+
+});
+Vue.config.devtools = true;
